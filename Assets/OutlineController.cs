07b2e74b41c7061abd9211fa6e77a6819891536d
Odivio <odiviobzr@gmail.com>
2021-10-30 18:42:28 -0300

    OutLine On/Off

---
+++ Assets/OutlineController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineController : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void setOutlineTrue()
+    {
+        gameObject.GetComponent<Outline>().enabled = true;
+    }
+
+    public void setOutlineFalse()
+    {
+        gameObject.GetComponent<Outline>().enabled = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

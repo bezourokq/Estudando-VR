e42be1e2a5942c51ea22132048eb3057704457a0
Odivio <odiviobzr@gmail.com>
2021-10-30 22:16:10 -0300

    Hand Collision

--- Assets/Hand_Controller.cs
+++ Assets/Hand_Controller.cs
@@ -7,26 +7,32 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Hand_Controller : MonoBehaviour
 {
     public GameObject hand_model;
-    public bool hand;
     private GameObject handGameObject;
     private InputDeviceCharacteristics ControllerC;
     private InputDevice targetDevice;
     private Animator handAnimator;
     private GameObject spawnedHand;
     private bool raycastOn;
+    private GameObject objectGR;
     // Start is called before the first frame update
     void Start()
     {
+        setUp();
+    }
+
+    void setUp()
+    {
         List<InputDevice> devices = new List<InputDevice>();
-        if (hand)
+
+        if (gameObject.name.Contains("Right"))
         {
             ControllerC = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-            handGameObject = GameObject.Find("Ray Interactor Right");
+            handGameObject = GameObject.Find("RightHand");
         }
         else
         {
             ControllerC = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
-            handGameObject = GameObject.Find("Ray Interactor Left");
+            handGameObject = GameObject.Find("LeftHand");
         }
         InputDevices.GetDevicesWithCharacteristics(ControllerC, devices);
         if (devices.Count > 0)
@@ -37,14 +43,13 @@ public class Hand_Controller : MonoBehaviour
         raycastOn = false;
 
         spawnedHand = Instantiate(hand_model, transform);
-        
+        spawnedHand.AddComponent<SphereCollider>();
+        spawnedHand.GetComponent<SphereCollider>().isTrigger = true;
+        spawnedHand.GetComponent<SphereCollider>().radius = 0.07f;
         //spawnedHand.AddComponent<Rigidbody>();
         //spawnedHand.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        //spawnedHand.AddComponent<SphereCollider>();
-        //spawnedHand.GetComponent<SphereCollider>().isTrigger = true;
-        //spawnedHand.GetComponent<SphereCollider>().radius = 0.05f;
-        handAnimator = spawnedHand.GetComponent<Animator>();
 
+        handAnimator = spawnedHand.GetComponent<Animator>();
 
     }
 
@@ -78,5 +83,30 @@ public class Hand_Controller : MonoBehaviour
     void Update()
     {
         UpdateHandAnimation();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        vibrate();
+        if (targetDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripValue) && gripValue)
+        {
+            if (collision.gameObject.tag.Contains("Interactable"))
+            {
+                collision.transform.parent = transform;
+                collision.rigidbody.isKinematic = true;
+                objectGR = collision.gameObject;
+            }
+        }
+        else
+        {
+            objectGR.GetComponent<Rigidbody>().isKinematic = false;
+            objectGR.transform.parent = null;
+        }
+    }
+
+    public void vibrate()
+    {
+        targetDevice.SendHapticImpulse(0, 1, 0.5f);
     }
 }
